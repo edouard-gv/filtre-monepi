d516a9603d0610c49c180327be9eeebe9b93f108
Edouard Gomez-Vaez <edouard@gomez-vaez.com>
2024-02-04 19:56:15 +0100

    Bouton de (dés)activation du filtre

--- chromium/content.js
+++ chromium/content.js
@@ -1,30 +1,47 @@
-if (!document.URL.startsWith("https://monepi.fr/boutique")) {
-    console.log("Pas dans monepi, on ne fait rien.");
-    console.log(document.URL);
-}
-else {
-
+if (document.URL.startsWith("https://monepi.fr/boutique") && document.getElementById('show-more-products-wrapper')) {
     function customFilterProducts() {
-        console.log("Dans customFilterProducts");
-        let productsRow = document.querySelector("#products-row");
-        for (let i = 0; i < productsRow.children.length; i++) {
-            const child = productsRow.children[i];
-            if (child.querySelector(".Product-CTA").classList.contains("disabled")) {
-                child.style.display = "none";
+        if (document.getElementById("custom-filter-button").classList.contains("light")) {
+            let productsRow = document.querySelector("#products-row");
+            for (let i = 0; i < productsRow.children.length; i++) {
+                const child = productsRow.children[i];
+                if (child.querySelector(".Product-CTA").classList.contains("disabled")) {
+                    child.style.display = "none";
+                }
             }
         }
     }
 
-    console.log("Dans monepi, on fait quelque chose.");
+    function cancelFilterProducts() {
+        let productsRow = document.querySelector("#products-row");
+        for (let i = 0; i < productsRow.children.length; i++) {
+            const child = productsRow.children[i];
+            if (child.style.display == "none") {
+                child.style.display = "block";
+            }
+        }
+    }
 
-    customFilterProducts();
+    console.log("Extension filtre monépi active sur cette page.");
 
     let button = document.createElement("button");
-    let li = document.createElement("li");
     button.innerText = "Filtrer";
+    button.id = "custom-filter-button";
+    button.classList.add("Button", "green", "small", "lowercased");
+    button.addEventListener('click', event => {
+        if (button.classList.contains("light")) {
+            button.classList.remove("light");
+            cancelFilterProducts();
+        }
+        else {
+            button.classList.add("light");
+        }
+    });
+    let li = document.createElement("li");
     button.addEventListener('click', customFilterProducts);
     li.appendChild(button);
     document.querySelector(".Nav-body").appendChild(li);
+
+    customFilterProducts();
 
     function handleScrollAndTouchMove() {
         if (document.getElementById("show-more-products-wrapper").style.display == "block" 
@@ -36,7 +53,7 @@ else {
     window.addEventListener('scroll', handleScrollAndTouchMove);
     window.addEventListener('touchmove', handleScrollAndTouchMove);
 
-    document.querySelector('#show-more-products-wrapper button').addEventListener('click', function(){
+    document.getElementById('show-more-products-wrapper').addEventListener('click', function(){
         customFilterProducts();
     });
 }
